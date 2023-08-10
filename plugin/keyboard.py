e9c3505a0f4683047d15d1d1c2ab7e7b67ceea4f
Carmen Zhang <jwzhang06@gmail.com>
2023-08-10 22:11:49 +0800

    keyboard

--- plugin/keyboard.py
+++ plugin/keyboard.py
@@ -7,7 +7,11 @@ key_siz = 60
 rax, ray = 120, 80
 history_press = None
 disabled = False
-keys = ['Q', 'W', 'E', 'R', 'T', 'Y',
+sizeX = 8
+sizeY = 5
+
+keys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+        'Q', 'W', 'E', 'R', 'T', 'Y',
         'U', 'I', 'O', 'P', 'A', 'S',
         'D', 'F', 'G', 'H', 'J', 'K',
         'L', 'Z', 'X', 'C', 'V', 'B',
@@ -22,16 +26,17 @@ def disable(d):
 def keyboard_print_rec(image):
     # 绘制键盘
     global history_press
-    for y in range(sty, 5 * ray + sty, ray):
+
+    for y in range(sty, sizeY * ray + sty, ray):
         index_y = (y - sty) // ray
-        for x in range(stx, 6 * rax + stx, rax):
+        for x in range(stx, sizeX * rax + stx, rax):
             index_x = (x - stx) // rax
-            if history_press and keys[index_y * 6 + index_x] == history_press:
+            if history_press and keys[index_y * sizeX + index_x] == history_press:
                 cv.rectangle(image, (x, y), (x + key_siz, y + key_siz), (255, 0, 0), 2)
                 history_press = None
             else:
                 cv.rectangle(image, (x, y), (x + key_siz, y + key_siz), (0, 255, 0), 2)
-            cv.putText(image, keys[index_y * 6 + index_x], (x + (key_siz // 2), y + (key_siz // 2)),
+            cv.putText(image, keys[index_y * sizeX + index_x], (x + (key_siz // 2), y + (key_siz // 2)),
                        cv.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2, cv.LINE_AA)
 
     return image
@@ -39,7 +44,7 @@ def keyboard_print_rec(image):
 
 def keyboard_on_key(pos, i, size):
     x, y = pos
-    ind_x, ind_y = (rax * (i % 6) + stx), (ray * (i // 6) + sty)
+    ind_x, ind_y = (rax * (i % sizeX) + stx), (ray * (i // sizeX) + sty)
     return ind_x <= x <= ind_x + size and ind_y <= y <= ind_y + size
 
 
